d24380924c956ebcadc4027178a08e8f2e325f1f
Nicolas Gajardo <n.gajardohernandez@gmail.com>
2017-10-27 22:25:24 -0300

    it checks the values

---
+++ _files/js/validations.js
@@ -0,0 +1,94 @@
+(function (win, doc) {
+  var methods = (function (win, doc) {
+    //TODO: msj functionality (use clousures)
+    function _validateField(field, fn, msj) {
+      function _changeInputColor(field, bol){
+        field.style.outlineStyle = "solid";
+        if(bol){
+            field.style.outlineColor = "green";
+        } else {
+          field.style.outlineColor = "red";
+        }
+      }
+      //return the validated Field 
+      var r = fn(field.value);
+      _changeInputColor(field, r);
+      return r;
+    }
+
+    function checkRut() {
+      _validateField(doc.getElementById('rut'), function(value){
+        //TODO:
+        return value && value.length === 9;
+      });
+    }
+    
+    function checkPassword(){
+      _validateField(doc.getElementById('password'), function(value){
+        return value && value.length === 4;
+      });
+    }
+    
+    function checkMount(){
+      _validateField(doc.getElementById('mount'), function(value){
+        return value && value >= 1;
+      });
+    }
+    
+    function checkSimulateButton(){
+      _validateField(doc.getElementById('simulate'), function(value){
+        return _checkAllSimularContent();
+      });
+      
+      function _checkAllSimularContent(){
+        function _checkSelects(ids){
+          var arrSelects = [];
+          
+          for (var i = 0; i < ids.length; i++) {
+            var isSelectCorrect = _validateField(doc.getElementById(ids[i]), function(value){
+              return value !== "Select";
+            });
+            arrSelects.push(isSelectCorrect);
+          }
+          
+          for (var j = 0; j < arrSelects.length; j++) {
+            if(!arrSelects[j]){
+              return arrSelects[j];
+            }
+          }
+          return true;
+        }
+        
+        _checkSelects(["select_PlazoPagar",
+            "select_PrimeraCuota",
+            "select_MesesNoPagoInicio",
+            "select_MesesNoPagoFin"]);
+            
+        checkRut();
+        checkMount();
+        
+        return false;
+      }
+    }
+    
+    function checkIngresar(){
+      return checkPassword() && checkRut();
+    } 
+    
+    return {
+      checkRut: checkRut,
+      checkPassword: checkPassword,
+      checkMount: checkMount,
+      checkSimulateButton: checkSimulateButton,
+      checkIngresar: checkIngresar
+    };
+  }(win, doc));
+  
+  win.checkRut = methods.checkRut;
+  win.checkPassword = methods.checkPassword;
+  win.checkMount = methods.checkMount;
+  win.checkSimulateButton = methods.checkSimulateButton;
+  win.checkIngresar = methods.checkIngresar;
+
+  
+}(window, document));
